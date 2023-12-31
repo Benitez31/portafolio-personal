a749036cf62e196085a0cc3663dcc95f9cc204fd
Benitez31 <95105127+Benitez31@users.noreply.github.com>
2023-12-31 12:34:36 -0300

    probando cambio en ruta

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -8,37 +8,37 @@ const Projects = () => {
       id: 1,
       title: 'Proyecto 1',
       description: 'Descripción del Proyecto 1',
-      url: 'src\images\web-viajes.mp4',
+      url: 'https://cdn.pixabay.com/vimeo/268112360/saigon-15952.mp4?width=640&hash=2b457c9d2035b8ef353bd1ac1670037c3bfda98b',
     },
     {
       id: 2,
       title: 'Proyecto 2',
       description: 'Descripción del Proyecto 2',
-      url: 'src/images/web-gaming.mp4',
+      url: 'src/images/web_gaming.mp4',
     },
     {
       id: 3,
       title: 'Proyecto 3',
       description: 'Descripción del Proyecto 3',
-      url: 'src/images/web-resto.mp4',
+      url: 'src/images/web_resto.mp4',
     },
     {
       id: 4,
       title: 'Proyecto 4',
       description: 'Descripción del Proyecto 4',
-      url: 'src/images/lista-de-tareas.mp4',
+      url: 'src/images/lista_de_tareas.mp4',
     },
     {
       id: 5,
       title: 'Proyecto 5',
       description: 'Descripción del Proyecto 5',
-      url: 'src/images/calculadora free.mp4',
+      url: 'src/images/calculadora_free.mp4',
     },
     {
       id: 6,
       title: 'Proyecto 6',
       description: 'Descripción del Proyecto 6',
-      url: 'src/images/boca-urna.mp4',
+      url: 'src/images/boca_urna.mp4',
     }
     ,
     {
