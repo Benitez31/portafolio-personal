dd279cc16dd8a82504d35c10e8eede7bf00fba4a
Benitez31 <95105127+Benitez31@users.noreply.github.com>
2023-12-31 20:02:32 -0300

    Update Projects.jsx

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -8,44 +8,44 @@ const Projects = () => {
       id: 1,
       title: 'Proyecto 1',
       description: 'Descripción del Proyecto 1',
-      url: 'https://cdn.pixabay.com/vimeo/268112360/saigon-15952.mp4?width=640&hash=2b457c9d2035b8ef353bd1ac1670037c3bfda98b',
+      url: 'https://www.xnxx.com/search-video/eAE9kd1uE0EMhV8FzbVJ_e-ZfQIQVVW15QJRtNr8NN0q2a3aFCIh3h0vIK5mdCx_59j-Wb6UrnwoUD7le57O5_x-LB2jmWIQlMvSfbVwUnECaRENOcAMm6gFeERgxUVpzB4GVIOJVUCR1agSRLVgawSqhlqbwH-8SNVK0kDNslkMgoRCW0BwVNVUnIXZJNK9NkQWMKVaYyEbIRotSgpGBpbWIdHAWILqAnR05sysTTwBmVlzBue2eAW25Airu-liqugaCt68ac6THDPKZFkiJ6-Y41AuZOkKpfCMal4rpz0QStZUv0H5nPu8v_g-bnfz-019Oj6Q3F9s33aHcdr3j8PLcZ7GzfDab-e39WHXrw_zj6d53Q_TeBxOu22_Hx_6zXx8Hg_DaZynPMt16RqUq9IhlLukv55ehnH_eFpKN3_U21T_8t7942XtLlUKzLnEVfO6ySmMtKKIlcdKuPz6DbplfIQ=/dabf976f6542ec4e254d2ecb8d90a288',
     },
     {
       id: 2,
       title: 'Proyecto 2',
       description: 'Descripción del Proyecto 2',
-      url: 'src/images/web_gaming.mp4',
+      url: 'https://www.xnxx.com/search/anal?id=32600217',
     },
     {
       id: 3,
       title: 'Proyecto 3',
       description: 'Descripción del Proyecto 3',
-      url: 'src/images/web_resto.mp4',
+      url: 'https://www.xnxx.com/search/sexy?id=55234697',
     },
     {
       id: 4,
       title: 'Proyecto 4',
       description: 'Descripción del Proyecto 4',
-      url: 'src/images/lista_de_tareas.mp4',
+      url: 'https://www.xnxx.com/search/gapes?id=69679563',
     },
     {
       id: 5,
       title: 'Proyecto 5',
       description: 'Descripción del Proyecto 5',
-      url: 'src/images/calculadora_free.mp4',
+      url: 'https://www.xnxx.com/search/big+black+cock?top&id=78869387',
     },
     {
       id: 6,
       title: 'Proyecto 6',
       description: 'Descripción del Proyecto 6',
-      url: 'src/images/boca_urna.mp4',
+      url: 'https://www.xnxx.com/search/que?top&id=78801093',
     }
     ,
     {
       id: 7,
       title: 'Proyecto 7',
       description: 'Descripción del Proyecto ',
-      url: 'src/images/plantilla.mp4',
+      url: 'https://www.xnxx.com/search/oral?top&id=60336237',
     }
 
   ];
